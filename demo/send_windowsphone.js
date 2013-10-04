49d98c2c16351a7879e131b135dac3c422aa912d
Guillaume Hilaire <hilaireguillaume@live.fr>
2013-10-04 12:30:11 +0200

    + debuging win phone

--- demo/send_windowsphone.js
+++ demo/send_windowsphone.js
@@ -7,10 +7,8 @@ var toto = Sender.send({
         msge : "Message du push Windows Phone"
     },
     title  : "titre du push windows phone",
-    tokens : ["http://db3.notify.live.net/throttledthirdparty/01.00/AAEABg_0lnViR4neLSbyVGgLAgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQ","http://db3.notify.live.net/throttledthirdparty/01.00/AQGc37w_JmNRTYL2Qlpir7-9AgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQFBkVVTk8wMQ","http://am3.notify.live.net/throttledthirdparty/01.00/AQFFm7vZbJDrSLdf5Y6h-8uCAgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQFBkVVV0UwMQ","http://am3.notify.live.net/throttledthirdparty/01.00/AQEqwtf00FpGQLzGGTtlOf65AgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQFBkVVV0UwMQ"],
-    config : {
-        apiKey : "GCM Api-KEY"
-    }
+    tokens : ["http://db3.notify.live.net/throttledthirdparty/01.00/AAEABg_0lnViR4neLSbyVGgLAgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQ","http://db3.notify.live.net/throttledthirdparty/01.00/AQGc37w_JmNRTYL2Qlpir7-9AgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQFBkVVTk8wMQ","http://am3.notify.live.net/throttledthirdparty/01.00/AQFFm7vZbJDrSLdf5Y6h-8uCAgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQFBkVVV0UwMQ","http://am3.notify.live.net/throttledthirdparty/01.00/AQEqwtf00FpGQLzGGTtlOf65AgAAAAADAQAAAAQUZm52OjIzOEQ2NDJDRkI5MEVFMEQFBkVVV0UwMQ"]
+
 }, function(err, response){
     console.log(err);
     console.log(response);
