765ce144f3e5865edc2e3189ed625be6b62f35b6
Guillaume Hilaire <hilaireguillaume@live.fr>
2013-10-30 13:48:43 +0100

    + debuging ssl push blackberry

--- demo/send_blackberry.js
+++ demo/send_blackberry.js
@@ -4,7 +4,6 @@ var https = require("https");
 https.globalAgent.options.secureProtocol = 'SSLv3_method';
 
 
-//http://localhost:3000/v1
 Sender.send({
     type : Sender.constants.TYPE_BB,
     message : {
@@ -19,10 +18,3 @@ Sender.send({
     console.log(err);
     console.log(response);
 });
-
-
-
-// prod : 40-82e7a73596il7rkI29933r3M8884y4a5R21e8
-//dev : 40-8729950B0i57RRR22o30c1M0t9y5a96o219k9
-
-//your token
